841f5f848af339564c0fedce74a0564b142ee248
trovtle basnet <basnet.trovtle@gmail.com>
2021-07-24 22:52:19 +1000

    Sass-Css Support added

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+import './styles/index.css';
+import './styles/newindex.scss';
+
 const numbers = { 1: [0, 1, 2], 2: [5, 0, 1, 2] };
 
 const newNumbers = [2, 32, 4];
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,15 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 let mode = 'development';
+let target = 'web';
 
 if (process.env.NODE_ENV === 'production') {
 	mode = 'production';
+	target = 'browserslist';
 }
 
 module.exports = {
 	mode,
+	target,
 
 	module: {
 		rules: [
@@ -14,11 +18,22 @@ module.exports = {
 				exclude: /node_modules/,
 				use: { loader: 'babel-loader' },
 			},
+			{
+				test: /\.s?css/,
+				use: [
+					MiniCssExtractPlugin.loader,
+					'css-loader',
+					'postcss-loader',
+					'sass-loader',
+				],
+			},
 		],
 	},
 
+	plugins: [new MiniCssExtractPlugin()],
 	devtool: 'source-map',
 	devServer: {
 		contentBase: './dist',
+		hot: true,
 	},
 };
